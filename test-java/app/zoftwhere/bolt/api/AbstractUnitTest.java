cb8cf974fc97eb231d13f099b44e63ee41f187f7
Osmund <21172332+osmundf@users.noreply.github.com>
2020-03-17 21:18:23 +0200

    Fix code style for AbstractUnitTest.

--- test-java/app/zoftwhere/bolt/api/AbstractUnitTest.java
+++ test-java/app/zoftwhere/bolt/api/AbstractUnitTest.java
@@ -2,13 +2,13 @@ package app.zoftwhere.bolt.api;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AbstractUnitTest {
 
     @Test
     void forCodeCoverage() {
-        var unit = new AbstractUnit(){};
+        var unit = new AbstractUnit() { };
         assertNotNull(unit);
     }
 
