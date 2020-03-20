38bd5cb92b103d747afe68ead09a000c601d73a4
Osmund <21172332+osmundf@users.noreply.github.com>
2020-03-20 17:54:59 +0200

    Added @inheritDoc and @since JavaDoc to Runner class (ensure @since visibility).

--- main-java/app/zoftwhere/bolt/Runner.java
+++ main-java/app/zoftwhere/bolt/Runner.java
@@ -35,66 +35,131 @@ public class Runner extends AbstractRunner {
     public Runner() {
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
     public RunnerProgram run(RunStandard program) {
         return new BoltProvideProgram().run(program);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
     public RunnerProgram run(Charset charset, RunStandard program) {
         return new BoltProvideProgram().run(charset, program);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
     public RunnerProgram runConsole(RunConsole program) {
         return new BoltProvideProgram().runConsole(program);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
     public RunnerProgram runConsole(Charset charset, RunConsole program) {
         return new BoltProvideProgram().runConsole(charset, program);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
     public RunnerPreProgram run(RunStandardArgued program) {
         return new BoltProvideProgram().run(program);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
     public RunnerPreProgram run(Charset charset, RunStandardArgued program) {
         return new BoltProvideProgram().run(charset, program);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
     public RunnerPreProgram runConsole(RunConsoleArgued program) {
         return new BoltProvideProgram().runConsole(program);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
     public RunnerPreProgram runConsole(Charset charset, RunConsoleArgued program) {
         return new BoltProvideProgram().runConsole(charset, program);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 1.0.0
+     */
     @Override
     public RunnerProgramInput input(String... input) {
         return new BoltProvideInput().input(input);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
-    public RunnerProgramInput input(InputStreamSupplier getInputStream) {
-        return new BoltProvideInput().input(getInputStream);
+    public RunnerProgramInput input(InputStreamSupplier supplier) {
+        return new BoltProvideInput().input(supplier);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 6.0.0
+     */
     @Override
-    public RunnerProgramInput input(InputStreamSupplier getInputStream, Charset charset) {
-        return new BoltProvideInput().input(getInputStream, charset);
+    public RunnerProgramInput input(InputStreamSupplier supplier, Charset charset) {
+        return new BoltProvideInput().input(supplier, charset);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 1.0.0
+     */
     @Override
     public RunnerProgramInput loadInput(String resourceName, Class<?> withClass) {
         return new BoltProvideInput().loadInput(resourceName, withClass);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * @since 1.0.0
+     */
     @Override
     public RunnerProgramInput loadInput(String resourceName, Class<?> withClass, Charset charset) {
         return new BoltProvideInput().loadInput(resourceName, withClass, charset);
