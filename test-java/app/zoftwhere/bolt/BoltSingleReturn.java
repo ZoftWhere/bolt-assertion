f90fb4230bf96e78c465b6cb41fb1de3ee797f81
Osmund <21172332+osmundf@users.noreply.github.com>
2020-04-11 12:01:50 +0200

    Update JavaDoc for test BoltSingleReturn class.

--- test-java/app/zoftwhere/bolt/BoltSingleReturn.java
+++ test-java/app/zoftwhere/bolt/BoltSingleReturn.java
@@ -2,6 +2,15 @@ package app.zoftwhere.bolt;
 
 import java.util.function.Supplier;
 
+/**
+ * <p>Bolt single return class.
+ * </p>
+ * <p>This is a test-only class for building lambda-based code blocks.
+ * </p>
+ *
+ * @param <T> The code block return type.
+ * @since 6.0.0
+ */
 public class BoltSingleReturn<T> {
 
     private final BoltPlaceHolder<T> boltPlaceHolder = new BoltPlaceHolder<>(null);
