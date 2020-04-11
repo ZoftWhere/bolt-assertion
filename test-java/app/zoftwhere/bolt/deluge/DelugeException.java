d27f9435eed82f687c5293a761ce91c3dc54cc78
Osmund <21172332+osmundf@users.noreply.github.com>
2020-04-11 12:00:26 +0200

    Updated warning suppression annotations.

--- test-java/app/zoftwhere/bolt/deluge/DelugeException.java
+++ test-java/app/zoftwhere/bolt/deluge/DelugeException.java
@@ -1,12 +1,12 @@
 package app.zoftwhere.bolt.deluge;
 
-@SuppressWarnings("WeakerAccess")
 public final class DelugeException extends RuntimeException {
 
     public DelugeException(String message) {
         super(message);
     }
 
+    @SuppressWarnings("WeakerAccess")
     public DelugeException(String message, Throwable cause) {
         super(message, cause);
     }
