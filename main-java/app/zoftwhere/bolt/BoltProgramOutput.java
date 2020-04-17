63765ed3d052d82b26529978d13bd621e028b121
Osmund <21172332+osmundf@users.noreply.github.com>
2020-04-17 10:13:27 +0200

    Merged BoltResult with BoltAsserter.

--- main-java/app/zoftwhere/bolt/BoltProgramOutput.java
+++ main-java/app/zoftwhere/bolt/BoltProgramOutput.java
@@ -1,11 +1,9 @@
 package app.zoftwhere.bolt;
 
-import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Objects;
 import java.util.Optional;
 
 import app.zoftwhere.bolt.api.RunnerAsserter;
@@ -13,7 +11,7 @@ import app.zoftwhere.bolt.api.RunnerInterface.InputStreamSupplier;
 import app.zoftwhere.bolt.api.RunnerPreTest;
 import app.zoftwhere.bolt.api.RunnerProgramOutput;
 
-import static app.zoftwhere.bolt.BoltReader.readArray;
+import static app.zoftwhere.bolt.BoltResult.newBoltResult;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Objects.requireNonNull;
 
@@ -94,134 +92,27 @@ class BoltProgramOutput implements RunnerProgramOutput {
     @Override
     public RunnerAsserter expected(String... expected) {
         String[] expectation = expected == null || expected.length == 0 ? new String[] {""} : expected;
-
-        if (error != null) {
-            return new BoltAsserter(new BoltResult(output, expectation, duration, error));
-        }
-
-        for (String item : expectation) {
-            if (item == null) {
-                RunnerException exception = new RunnerException("bolt.runner.variable.array.expected.has.null");
-                return new BoltAsserter(new BoltResult(output, expectation, duration, exception));
-            }
-        }
-
-        return new BoltAsserter(buildTestResult(output, expectation, comparator));
+        return BoltResult.newBoltResult(output, expectation, duration, comparator, error);
     }
 
     @Override
     public RunnerAsserter expected(InputStreamSupplier supplier) {
-        return expected(supplier, UTF_8);
+        return BoltResult.newBoltResult(output, supplier, UTF_8, duration, comparator, error);
     }
 
     @Override
     public RunnerAsserter expected(InputStreamSupplier supplier, Charset charset) {
-        if (error != null) {
-            return new BoltAsserter(new BoltResult(output, new String[0], duration, error));
-        }
-
-        return create(charset, supplier);
+        return BoltResult.newBoltResult(output, supplier, charset, duration, comparator, error);
     }
 
     @Override
     public RunnerAsserter loadExpectation(String resourceName, Class<?> withClass) {
-        if (error != null) {
-            return new BoltAsserter(new BoltResult(output, new String[0], duration, error));
-        }
-
-        return loadExpectation(resourceName, withClass, UTF_8);
+        return newBoltResult(output, resourceName, withClass, UTF_8, duration, comparator, error);
     }
 
     @Override
     public RunnerAsserter loadExpectation(String resourceName, Class<?> withClass, Charset charset) {
-        if (error != null) {
-            return new BoltAsserter(new BoltResult(output, new String[0], duration, error));
-        }
-        if (resourceName == null) {
-            return create(charset, () -> {
-                throw new RunnerException("bolt.runner.load.expectation.resource.name.null");
-            });
-        }
-        if (withClass == null) {
-            return create(charset, () -> {
-                throw new RunnerException("bolt.runner.load.expectation.resource.class.null");
-            });
-        }
-        if (withClass.getResource(resourceName) == null) {
-            return create(charset, () -> {
-                throw new RunnerException("bolt.runner.load.expectation.resource.not.found");
-            });
-        }
-
-        return create(charset, () -> withClass.getResourceAsStream(resourceName));
-    }
-
-    /**
-     * Helper method to create a {@link RunnerAsserter} for the expected result {@link InputStream}.
-     *
-     * @param charset  character encoding of {@link InputStream}
-     * @param supplier {@link InputStream} supplier for the expected program result
-     * @return a {@link RunnerAsserter} instance
-     */
-    private RunnerAsserter create(Charset charset, InputStreamSupplier supplier) {
-        if (charset == null) {
-            RunnerException exception = new RunnerException("bolt.runner.load.expectation.charset.null");
-            BoltResult result = new BoltResult(output, new String[0], duration, exception);
-            return new BoltAsserter(result);
-        }
-        if (supplier == null) {
-            RunnerException exception = new RunnerException("bolt.runner.load.expectation.supplier.null");
-            BoltResult result = new BoltResult(output, new String[0], duration, exception);
-            return new BoltAsserter(result);
-        }
-
-        try (InputStream inputStream = supplier.get()) {
-            if (inputStream == null) {
-                throw new RunnerException("bolt.runner.load.expectation.stream.null");
-            }
-            final String[] expected = readArray(() -> new BoltReader(inputStream, charset));
-            return new BoltAsserter(buildTestResult(output, expected, comparator));
-        }
-        catch (Exception e) {
-            BoltResult result = new BoltResult(output, new String[0], duration, e);
-            return new BoltAsserter(result);
-        }
-    }
-
-    /**
-     * Helper method to build a {@link BoltResult} instance.
-     *
-     * @param output     the actual program output
-     * @param expected   the expected program output
-     * @param comparator {@code Nullable} comparator
-     * @return {@link BoltResult}
-     */
-    private BoltResult buildTestResult(String[] output, String[] expected, Comparator<String> comparator) {
-        if (expected.length != output.length) {
-            int none = -1;
-            String message = "bolt.runner.asserter.output.length.mismatch";
-            return new BoltResult(output, expected, duration, none, message);
-        }
-
-        final int size = output.length;
-        if (comparator == null) {
-            for (int index = 0; index < size; index++) {
-                if (!Objects.equals(expected[index], output[index])) {
-                    String message = "bolt.runner.asserter.output.data.mismatch";
-                    return new BoltResult(output, expected, duration, index, message);
-                }
-            }
-        }
-        else {
-            for (int index = 0; index < size; index++) {
-                if (comparator.compare(expected[index], output[index]) != 0) {
-                    String message = "bolt.runner.asserter.output.data.mismatch";
-                    return new BoltResult(output, expected, duration, index, message);
-                }
-            }
-        }
-
-        return new BoltResult(output, expected, duration);
+        return newBoltResult(output, resourceName, withClass, charset, duration, comparator, error);
     }
 
 }
--- test-java/app/zoftwhere/bolt/BoltResultTest.java
+++ test-java/app/zoftwhere/bolt/BoltResultTest.java
@@ -4,10 +4,12 @@ import java.time.Duration;
 
 import org.junit.jupiter.api.Test;
 
+import static app.zoftwhere.bolt.BoltTestHelper.array;
 import static app.zoftwhere.bolt.BoltTestHelper.assertClass;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -20,32 +22,325 @@ class BoltResultTest {
     private final Duration instant = Duration.ZERO;
 
     @Test
+    void testSuccessState() {
+        final var blank = new String[] {""};
+        final var fire = new BoltResult(blank, blank, instant);
+
+        fire.assertSuccess();
+
+        assertTrue(fire.isSuccess());
+        assertFalse(fire.isFailure());
+        assertFalse(fire.isError());
+
+        assertNull(fire.message().orElse(null));
+        assertNull(fire.error().orElse(null));
+
+        try {
+            fire.assertFailure();
+            fail("bolt.runner.asserter.success.found.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.asserter.success.found", e.getMessage());
+            assertNull(e.getCause());
+        }
+
+        try {
+            fire.assertError();
+            fail("bolt.runner.asserter.success.found.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.asserter.success.found", e.getMessage());
+            assertNull(e.getCause());
+        }
+
+        try {
+            fire.assertCheck(runnerResult -> {
+                assertTrue(runnerResult.isSuccess());
+                assertFalse(runnerResult.isFailure());
+                assertFalse(runnerResult.isError());
+                assertNull(runnerResult.message().orElse(null));
+                assertNull(runnerResult.error().orElse(null));
+                throw new Exception("bolt.asserter.assert.check.test");
+            });
+        } catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.assert.check", e.getMessage());
+            assertClass(Exception.class, e.getCause());
+            assertEquals("bolt.asserter.assert.check.test", e.getCause().getMessage());
+        }
+
+        fire.onOffence(runnerResult -> {
+            throw new RunnerException("bolt.runner.asserter.fall.through.expected");
+        });
+    }
+
+    @Test
+    void testFailureState() {
+        final var blank = new String[] {""};
+        final var customMessage = "bolt.asserter.custom.message";
+        final var fire = new BoltResult(blank, blank, instant, -1, customMessage);
+
+        fire.assertFailure();
+
+        assertFalse(fire.isSuccess());
+        assertTrue(fire.isFailure());
+        assertFalse(fire.isError());
+
+        assertNotNull(fire.message().orElse(null));
+        assertNull(fire.error().orElse(null));
+
+        try {
+            fire.assertSuccess();
+            fail("bolt.runner.asserter.failure.found.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals(customMessage, e.getMessage());
+            assertNull(e.getCause());
+        }
+
+        try {
+            fire.assertError();
+            fail("bolt.runner.asserter.success.found.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals(customMessage, e.getMessage());
+            assertNull(e.getCause());
+        }
+
+        try {
+            fire.assertCheck(runnerResult -> {
+                assertFalse(runnerResult.isSuccess());
+                assertTrue(runnerResult.isFailure());
+                assertFalse(runnerResult.isError());
+                assertNotNull(runnerResult.message().orElse(null));
+                assertNull(runnerResult.error().orElse(null));
+                throw new Exception("bolt.asserter.assert.check.test");
+            });
+        } catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.assert.check", e.getMessage());
+            assertClass(Exception.class, e.getCause());
+            assertEquals("bolt.asserter.assert.check.test", e.getCause().getMessage());
+        }
+
+        try {
+            fire.onOffence(runnerResult -> {
+                throw new Exception("bolt.asserter.on.offence.test");
+            });
+        } catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.on.offence", e.getMessage());
+            assertClass(Exception.class, e.getCause());
+            assertEquals("bolt.asserter.on.offence.test", e.getCause().getMessage());
+        }
+    }
+
+    @Test
+    void testErrorState() {
+        final var blank = new String[] {""};
+        final var empty = new String[0];
+        final var errorMessage = "Throwable?";
+        final var exception = new Exception(errorMessage, null);
+        final var fire = new BoltResult(blank, empty, instant, exception);
+
+        fire.assertError();
+
+        assertFalse(fire.isSuccess());
+        assertFalse(fire.isFailure());
+        assertTrue(fire.isError());
+
+        assertNull(fire.message().orElse(null));
+
+        Exception error = fire.error().orElse(null);
+        assertNotNull(error);
+        assertClass(exception.getClass(), error);
+        assertEquals("Throwable?", error.getMessage());
+
+        try {
+            fire.assertSuccess();
+            fail("bolt.runner.asserter.error.found.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.asserter.error.found", e.getMessage());
+            assertNull(e.getCause());
+        }
+
+        try {
+            fire.assertFailure();
+            fail("bolt.runner.asserter.error.found.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.asserter.error.found", e.getMessage());
+            assertNull(e.getCause());
+        }
+
+        try {
+            fire.assertCheck(runnerResult -> {
+                assertFalse(runnerResult.isSuccess());
+                assertFalse(runnerResult.isFailure());
+                assertTrue(runnerResult.isError());
+                assertNull(runnerResult.message().orElse(null));
+                assertNotNull(runnerResult.error().orElse(null));
+                throw new Exception("bolt.asserter.assert.check.test");
+            });
+        } catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.assert.check", e.getMessage());
+            assertClass(Exception.class, e.getCause());
+            assertEquals("bolt.asserter.assert.check.test", e.getCause().getMessage());
+        }
+
+        try {
+            fire.onOffence(runnerResult -> {
+                throw new Exception("bolt.asserter.on.offence.test");
+            });
+        } catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.on.offence", e.getMessage());
+            assertClass(Exception.class, e.getCause());
+            assertEquals("bolt.asserter.on.offence.test", e.getCause().getMessage());
+        }
+    }
+
+    @Test
+    void testRunTestFailureExpectedLength() {
+        var outputLines = array("");
+        var expectedLines = array("", "");
+        final var programOutput = new BoltProgramOutput(outputLines, instant, null);
+        final var fire = programOutput.expected(expectedLines);
+
+        try {
+            fire.assertSuccess();
+            fail("bolt.runner.test.exception.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.asserter.output.length.mismatch", e.getMessage());
+        }
+
+        fire.assertFailure();
+
+        try {
+            fire.assertError();
+            fail("bolt.runner.test.exception.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.asserter.output.length.mismatch", e.getMessage());
+        }
+
+        try {
+            fire.onOffence(result -> {
+                throw new IllegalStateException(result.message().orElse("null.message"));
+            });
+            fail("bolt.runner.test.exception.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.on.offence", e.getMessage());
+            assertNotNull(e.getCause());
+            assertClass(IllegalStateException.class, e.getCause());
+            assertEquals("bolt.runner.asserter.output.length.mismatch", e.getCause().getMessage());
+        }
+
+        var result = fire.result();
+        assertEquals(-1, result.offendingIndex());
+        assertNull(result.error().orElse(null));
+        assertEquals("bolt.runner.asserter.output.length.mismatch", result.message().orElse(null));
+    }
+
+    @Test
+    void testRunTestFailureComparisonFailure() {
+        var outputLines = array("");
+        var expectedLines = array("mismatch");
+        final var programOutput = new BoltProgramOutput(outputLines, instant, null);
+        final var fire = programOutput.expected(expectedLines);
+
+        try {
+            fire.assertSuccess();
+            fail("bolt.runner.test.exception.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.asserter.output.data.mismatch", e.getMessage());
+        }
+
+        fire.assertFailure();
+
+        try {
+            fire.assertError();
+            fail("bolt.runner.test.exception.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.asserter.output.data.mismatch", e.getMessage());
+        }
+
+        try {
+            fire.onOffence(result -> {
+                throw new IllegalStateException(result.message().orElse(null), null);
+            });
+            fail("bolt.runner.test.exception.expected");
+        }
+        catch (Exception e) {
+            assertClass(RunnerException.class, e);
+            assertEquals("bolt.runner.on.offence", e.getMessage());
+            assertNotNull(e.getCause());
+            assertClass(IllegalStateException.class, e.getCause());
+            assertEquals("bolt.runner.asserter.output.data.mismatch", e.getCause().getMessage());
+        }
+
+        var result = fire.result();
+        assertEquals(0, result.offendingIndex());
+        assertNull(result.error().orElse(null));
+        assertEquals("bolt.runner.asserter.output.data.mismatch", result.message().orElse(null));
+    }
+
+    @Test
+    void testOnOffenceFallThrough() {
+        final var one = new String[] {"one"};
+        final var two = new String[] {"two"};
+        final var offendingIndex = 0;
+        final var message = "mismatch";
+        final var fire = new BoltResult(one, two, instant, offendingIndex, message);
+
+        fire.onOffence(runnerResult -> {});
+    }
+
+
+    @Test
     void testForSuccessState() {
         var inputArray = new String[][] {null, nullArray, emptyArray, blankArray};
         for (String[] outputStringArray : inputArray) {
             for (String[] expectedStringArray : inputArray) {
                 try {
-                    var result = new BoltResult(outputStringArray, expectedStringArray, instant);
+                    var fire = new BoltResult(outputStringArray, expectedStringArray, instant);
 
                     if (outputStringArray == null || expectedStringArray == null) {
                         fail("null.pointer.exception.expected");
                     }
 
-                    assertTrue(result.isSuccess());
-                    assertFalse(result.isFailure());
-                    assertFalse(result.isError());
+                    assertTrue(fire.isSuccess());
+                    assertFalse(fire.isFailure());
+                    assertFalse(fire.isError());
 
-                    String[] output = result.output();
-                    String[] expected = result.expected();
-                    int offendingIndex = result.offendingIndex();
-                    String message = result.message().orElse(null);
-                    Exception error = result.error().orElse(null);
+                    String[] output = fire.output();
+                    String[] expected = fire.expected();
+                    int offendingIndex = fire.offendingIndex();
+                    String message = fire.message().orElse(null);
+                    Exception error = fire.error().orElse(null);
 
                     assertEquals(outputStringArray.length, output.length);
                     assertEquals(expectedStringArray.length, expected.length);
                     assertArrayEquals(outputStringArray, output);
                     assertArrayEquals(expectedStringArray, expected);
-                    assertEquals(instant, result.executionDuration());
+                    assertEquals(instant, fire.executionDuration());
                     assertEquals(-1, offendingIndex);
                     assertNull(message);
                     assertNull(error);
