790168931239259c77c055eae909029f7c3d39f6
Osmund <21172332+osmundf@users.noreply.github.com>
2020-04-21 20:12:38 +0200

    Updated DelugeMock test class variable names for convention.

--- test-java/app/zoftwhere/bolt/deluge/DelugeMock.java
+++ test-java/app/zoftwhere/bolt/deluge/DelugeMock.java
@@ -26,26 +26,26 @@ import static app.zoftwhere.bolt.deluge.DelugeDataType.STREAM_ENCODED;
 
 class DelugeMock {
 
-    static DelugeMock from(DelugeProgramType type, DelugeSetting setting, DelugeData data) {
-        return new DelugeMock(type, setting, data);
+    static DelugeMock from(DelugeProgramType type, DelugeSetting setting, DelugeData input) {
+        return new DelugeMock(type, setting, input);
     }
 
     private final DelugeBuilder builder;
-    private final DelugeProgramType programType;
+    private final DelugeProgramType type;
     private final DelugeSetting setting;
-    private final DelugeData data;
+    private final DelugeData input;
 
-    private DelugeMock(DelugeProgramType programType, DelugeSetting setting, DelugeData data) {
-        this.builder = DelugeBuilder.from(programType, setting, data);
-        this.programType = programType;
+    private DelugeMock(DelugeProgramType type, DelugeSetting setting, DelugeData input) {
+        this.builder = DelugeBuilder.from(type, setting, input);
+        this.type = type;
         this.setting = setting;
-        this.data = data;
+        this.input = input;
     }
 
     DelugeProgramOutput buildExpectedOutput() {
 
-        if (data.hasCharset()) {
-            if (data.charset() == null) {
+        if (input.hasCharset()) {
+            if (input.charset() == null) {
                 String exceptionMessage = "bolt.runner.input.charset.null";
                 Exception error = new RunnerException(exceptionMessage, null);
                 return DelugeProgramOutput.from(array(""), Duration.ZERO, error);
@@ -58,34 +58,34 @@ class DelugeMock {
             return DelugeProgramOutput.from(array(""), Duration.ZERO, error);
         }
 
-        if (ARRAY == data.type() || ARRAY_ENCODED == data.type()) {
-            if (data.array() == null) {
+        if (ARRAY == input.type() || ARRAY_ENCODED == input.type()) {
+            if (input.array() == null) {
                 String exceptionMessage = "bolt.runner.variable.argument.input.null";
                 Exception error = new RunnerException(exceptionMessage, null);
                 return DelugeProgramOutput.from(array(""), Duration.ZERO, error);
             }
 
-            if (arrayHasNull(data.array())) {
+            if (arrayHasNull(input.array())) {
                 String exceptionMessage = "bolt.runner.variable.argument.input.has.null";
                 Exception error = new RunnerException(exceptionMessage, null);
                 return DelugeProgramOutput.from(array(""), Duration.ZERO, error);
             }
         }
 
-        if (RESOURCE == data.type() || RESOURCE_ENCODED == data.type()) {
-            if (data.resource() == null) {
+        if (RESOURCE == input.type() || RESOURCE_ENCODED == input.type()) {
+            if (input.resource() == null) {
                 String exceptionMessage = "bolt.runner.load.input.resource.name.null";
                 Exception error = new RunnerException(exceptionMessage, null);
                 return DelugeProgramOutput.from(array(""), Duration.ZERO, error);
             }
 
-            if (data.withClass() == null) {
+            if (input.withClass() == null) {
                 String exceptionMessage = "bolt.runner.load.input.resource.class.null";
                 Exception error = new RunnerException(exceptionMessage, null);
                 return DelugeProgramOutput.from(array(""), Duration.ZERO, error);
             }
 
-            final var url = data.withClass().getResource(data.resource());
+            final var url = input.withClass().getResource(input.resource());
             if (url == null) {
                 String exceptionMessage = "bolt.runner.load.input.resource.not.found";
                 Exception error = new RunnerException(exceptionMessage, null);
@@ -93,18 +93,18 @@ class DelugeMock {
             }
         }
 
-        if (STREAM == data.type() || STREAM_ENCODED == data.type()) {
-            if (data.error() != null) {
-                return DelugeProgramOutput.from(array(""), Duration.ZERO, data.error());
+        if (STREAM == input.type() || STREAM_ENCODED == input.type()) {
+            if (input.error() != null) {
+                return DelugeProgramOutput.from(array(""), Duration.ZERO, input.error());
             }
 
-            if (data.array() == null) {
+            if (input.array() == null) {
                 String exceptionMessage = "bolt.runner.input.stream.supplier.null";
                 Exception error = new RunnerException(exceptionMessage, null);
                 return DelugeProgramOutput.from(array(""), Duration.ZERO, error);
             }
 
-            if (isOrHasNull(data.array())) {
+            if (isOrHasNull(input.array())) {
                 String exceptionMessage = "bolt.runner.load.input.input.stream.null";
                 Exception error = new RunnerException(exceptionMessage, null);
                 return DelugeProgramOutput.from(array(""), Duration.ZERO, error);
@@ -114,8 +114,8 @@ class DelugeMock {
         if (setting.hasError()) {
             return DelugeProgramOutput.from(array(""), Duration.ofDays(1), setting.error());
         }
-        if (data.hasError()) {
-            return DelugeProgramOutput.from(array(""), Duration.ofDays(1), data.error());
+        if (input.hasError()) {
+            return DelugeProgramOutput.from(array(""), Duration.ofDays(1), input.error());
         }
 
         return buildOutput();
@@ -128,7 +128,7 @@ class DelugeMock {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream out = new PrintStream(outputStream, false, outEnc);
 
-        if (!programType.isArgued()) {
+        if (!type.isArgued()) {
             out.println("Argument: <null>");
         }
         else if (setting.argumentArray() == null) {
@@ -172,23 +172,23 @@ class DelugeMock {
     }
 
     private InputStreamSupplier newSupplier() {
-        if (ARRAY == data.type()) {
-            if (data.array() == null) {
+        if (ARRAY == input.type()) {
+            if (input.array() == null) {
                 return () -> new ByteArrayInputStream(new byte[0]);
             }
 
             Charset charset = builder.inputCharset();
-            return data.newInputStreamSupplier(charset);
+            return input.newInputStreamSupplier(charset);
         }
 
-        return data.streamSupplier();
+        return input.streamSupplier();
     }
 
     private Scanner newScanner(InputStream inputStream, Charset inEnc, Charset outEnc) {
-        if (programType.isStandard()) {
+        if (type.isStandard()) {
             return new Scanner(inputStream, builder.inputCharset());
         }
-        else if (programType.isConsole()) {
+        else if (type.isConsole()) {
             return new Scanner(transcode(inputStream, inEnc, outEnc), outEnc);
         }
         else {
