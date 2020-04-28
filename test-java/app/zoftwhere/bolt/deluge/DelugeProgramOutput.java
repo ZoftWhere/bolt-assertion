c33ffd8a7bdeb1138884e6112e0413fa728aeb23
Osmund <21172332+osmundf@users.noreply.github.com>
2020-04-28 10:33:49 +0200

    Updated DelugeProgramOutput test class with weaker access method and implicit-var definitions.

--- test-java/app/zoftwhere/bolt/deluge/DelugeProgramOutput.java
+++ test-java/app/zoftwhere/bolt/deluge/DelugeProgramOutput.java
@@ -6,15 +6,14 @@ import app.zoftwhere.bolt.api.RunnerProgramOutput;
 
 public class DelugeProgramOutput {
 
-    @SuppressWarnings("WeakerAccess")
-    public static DelugeProgramOutput from(String[] output, Duration duration, Exception error) {
+    public static DelugeProgramOutput from(RunnerProgramOutput programOutput) {
+        var output = programOutput.output();
+        var duration = programOutput.executionDuration();
+        var error = programOutput.error().orElse(null);
         return new DelugeProgramOutput(output, duration, error);
     }
 
-    public static DelugeProgramOutput from(RunnerProgramOutput programOutput) {
-        String[] output = programOutput.output();
-        Duration duration = programOutput.executionDuration();
-        Exception error = programOutput.error().orElse(null);
+    static DelugeProgramOutput from(String[] output, Duration duration, Exception error) {
         return new DelugeProgramOutput(output, duration, error);
     }
 
