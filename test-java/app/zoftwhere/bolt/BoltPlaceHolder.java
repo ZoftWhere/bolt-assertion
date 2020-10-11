6b1c293a399f1b38af6e82f4ce6c641cad15c9cc
Osmund <21172332+osmundf@users.noreply.github.com>
2020-10-11 21:30:05 +0200

    Added JavaDoc for BoltPlaceHolder test class.

--- test-java/app/zoftwhere/bolt/BoltPlaceHolder.java
+++ test-java/app/zoftwhere/bolt/BoltPlaceHolder.java
@@ -1,26 +1,42 @@
 package app.zoftwhere.bolt;
 
 /**
- * <p>Bolt place holder class.
+ * <p>Bolt Place Holder class.
  * </p>
  * <p>This is a test-only class for being able to pass mutable objects as parameters.
  * </p>
  *
  * @param <T> The data type.
+ * @author Osmund
  * @since 6.0.0
  */
 public class BoltPlaceHolder<T> {
 
     private T value;
 
+    /**
+     * <p>Constructor for BoltPlaceHolder.</p>
+     *
+     * @param value a T object.
+     */
     public BoltPlaceHolder(T value) {
         this.value = value;
     }
 
+    /**
+     * Setter for the mutable value.
+     *
+     * @param value a T object.
+     */
     public void set(T value) {
         this.value = value;
     }
 
+    /**
+     * Getter for the mutable value.
+     *
+     * @return a T object.
+     */
     public T get() {
         return value;
     }
